1d758223368140d38c6f76e1fd73fa3532c85869
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-06-01 22:24:40 +0200

    Add SignIn component

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@chakra-ui/button";
+import { FormControl } from "@chakra-ui/form-control";
+import { Container, Text } from "@chakra-ui/layout";
+import { Select } from "@chakra-ui/select";
+import { FC, FormEvent } from "react";
+import { setAuthedUser } from "../redux/actions/authedUser";
+import { useAppDispatch, useAppSelector } from "../utils/hooks";
+
+interface SignInProps {}
+const SignIn: FC<SignInProps> = (props) => {
+  const dispatch = useAppDispatch();
+  const users = useAppSelector((state) => state.users);
+
+  function handleSubmit(evt: FormEvent<HTMLFormElement>) {
+    evt.preventDefault();
+    const user = new FormData(evt.currentTarget).get("user");
+    dispatch(setAuthedUser(user as string));
+  }
+
+  return (
+    <Container textAlign="center" maxW="container.sm" py="16">
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <Text
+            as="legend"
+            fontSize="2xl"
+            mb="5"
+            color="teal"
+            fontWeight="bold"
+          >
+            Sign In
+          </Text>
+          <FormControl mb="3">
+            <Select placeholder="Select a User" name="user" required>
+              {Object.values(users).map((user) => (
+                <option key={user.id} value={user.id}>
+                  {user.name}
+                </option>
+              ))}
+            </Select>
+          </FormControl>
+          <Button colorScheme="teal" w="100%" type="submit">
+            Sign In
+          </Button>
+        </fieldset>
+      </form>
+    </Container>
+  );
+};
+
+export default SignIn;
